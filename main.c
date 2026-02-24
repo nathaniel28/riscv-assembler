228c25811439a68dde44982bf9e33634f4f43b4f
Nathaniel <n34@heidemann.la.ca.us>
2026-02-23 16:37:27 -0800

    trie uses offset into auxiliary buffer instead of pointer for data and next

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ instructions
 typedef struct {
 	uint64_t keys[2];
 	uint64_t terms[2];
-	int *next;
-	int *data;
+	int next;
+	int data;
 } trie;
 
 // this trie is kinda strange: it keeps a bitset keys and terminals
@@ -44,23 +44,23 @@ int trie_next(trie *t, unsigned char c) {
 		in = __builtin_popcountl(t->keys[0]);
 	}
 	in += __builtin_popcountl(t->keys[ik] << (64 - frag));
-	return t->next[in];
+	return t->next + in;
 }
 
-int *trie_term(trie *t, unsigned char c) {
+int trie_term(trie *t, unsigned char c) {
 	int ik = (c >> 6) & 1; // lo or hi part of keys
 	int frag = c & 63; // bit index of keys[ik]
 	if (((t->terms[ik] >> frag) & 1) == 0) {
-		return NULL;
+		return -1;
 	}
 	int in = 0;
 	// ensure popcountl is suitable for u64 (it probably is)
 	static_assert(sizeof(unsigned long) == sizeof(uint64_t));
 	if (ik) {
-		in = __builtin_popcountl(t->keys[0]);
+		in = __builtin_popcountl(t->terms[0]);
 	}
-	in += __builtin_popcountl(t->keys[ik] << (64 - frag));
-	return &t->data[in];
+	in += __builtin_popcountl(t->terms[ik] << (64 - frag));
+	return t->data + in;
 }
 
 void trie_set_key_(uint64_t keys[2], unsigned char c) {
@@ -84,7 +84,7 @@ typedef struct trie_builder {
 void *calloc_(size_t sz) {
 	void *res = calloc(1, sz);
 	if (!res) {
-		puts("out of memory");
+		printf("out of memory\n");
 		exit(1);
 	}
 	return res;
@@ -93,20 +93,20 @@ void *calloc_(size_t sz) {
 void *realloc_(void *ptr, size_t sz) {
 	void *res = realloc(ptr, sz);
 	if (!res) {
-		puts("out of memory");
+		printf("out of memory\n");
 		exit(1);
 	}
 	return res;
 }
 
-int _trie_builder_insert(trie_builder *base, const char *str, int data) {
+int trie_builder_insert(trie_builder *base, const char *str, int data) {
 	char c = *str;
 	if (!c) {
 		return 0;
 	}
 	for (int i = 0; i < base->len; i++) {
 		if (base->ents[i]->key == c) {
-			return _trie_builder_insert(base->ents[i]->next, str + 1, data);
+			return trie_builder_insert(base->ents[i]->next, str + 1, data);
 		}
 	}
 	trie_builder_entry *nu = calloc_(sizeof(trie_builder_entry));
@@ -117,14 +117,14 @@ int _trie_builder_insert(trie_builder *base, const char *str, int data) {
 	base->ents = realloc_(base->ents, sizeof(trie_builder_entry *) * base->len);
 	base->ents[base->len - 1] = nu;
 	if (str[1]) {
-		return _trie_builder_insert(nu->next, str + 1, data) + 1;
+		return trie_builder_insert(nu->next, str + 1, data) + 1;
 	}
 	nu->data = data;
 	nu->term = 1;
 	return 0;
 }
 
-void _print_keys(uint64_t keys[2]) {
+void print_keys_(uint64_t keys[2]) {
 	uint64_t x = keys[0];
 	for (int i = 0; i < 64; i++) {
 		if (x & 1) {
@@ -142,11 +142,17 @@ void _print_keys(uint64_t keys[2]) {
 }
 
 trie *gbuf;
+int *aux;
+int apos = 0;
 int gpos = 0;
 
 void gbuf_size(trie_builder *base) {
 	for (int i = 0; i < base->len; i++) {
+		if (base->ents[i]->term) {
+			apos++;
+		}
 		if (base->ents[i]->next->len > 0) {
+			apos++;
 			gpos++;
 			gbuf_size(base->ents[i]->next);
 		}
@@ -155,8 +161,6 @@ void gbuf_size(trie_builder *base) {
 
 void gbuf_fill(trie_builder *base) {
 	int n = gpos;
-	gbuf[n].keys[0] = 0;
-	gbuf[n].keys[1] = 0;
 	int np = 0;
 	int dp = 0;
 	for (int i = 0; i < base->len; i++) {
@@ -167,38 +171,53 @@ void gbuf_fill(trie_builder *base) {
 			np++;
 		}
 	}
-	gbuf[n].next = calloc_(sizeof(int) * np);
-	gbuf[n].data = calloc_(sizeof(int) * dp);
-	np = 0;
-	dp = 0;
+	gbuf[n].keys[0] = 0;
+	gbuf[n].keys[1] = 0;
+	gbuf[n].terms[0] = 0;
+	gbuf[n].terms[1] = 0;
+	if (np == 0) {
+		gbuf[n].next = 0; // can really be anything
+	} else {
+		gbuf[n].next = apos;
+	}
+	if (dp == 0) {
+		gbuf[n].data = 0; // can really be anything
+	} else {
+		gbuf[n].data = apos + np;
+	}
+	apos += np + dp;
+	np = gbuf[n].next;
+	dp = gbuf[n].data;
 	for (int i = 0; i < base->len; i++) {
 		unsigned char k = base->ents[i]->key;
 		if (base->ents[i]->term) {
 			trie_set_key_(gbuf[n].terms, k);
-			gbuf[n].data[dp++] = base->ents[i]->data;
+			aux[dp++] = base->ents[i]->data;
 		}
 		if (base->ents[i]->next->len > 0) {
 			trie_set_key_(gbuf[n].keys, k);
 			gpos++;
-			gbuf[n].next[np++] = gpos;
+			aux[np++] = gpos;
 			gbuf_fill(base->ents[i]->next);
 		}
 	}
 
+	/*
 	printf("links: ");
 	for (int i = 0; i < np; i++) {
 		printf("%d ", gbuf[n].next[i]);
 	}
 	printf("@ %d: ", n);
-	_print_keys(gbuf[n].keys);
+	print_keys_(gbuf[n].keys);
 	printf("/");
-	_print_keys(gbuf[n].terms);
-	puts("");
+	print_keys_(gbuf[n].terms);
+	printf("\n");
+	*/
 }
 
 int main() {
 	static trie_builder base;
-#define A(x, d) _trie_builder_insert(&base, x, d)
+#define A(x, d) trie_builder_insert(&base, x, d)
 	A("add", ADD);
 	A("sub", SUB);
 	A("xor", XOR);
@@ -247,92 +266,27 @@ int main() {
 	A("ebreak", EBREAK);
 
 	gpos = 0;
+	apos = 0;
 	gbuf_size(&base);
-	printf("pos: %d\n", gpos);
-	gbuf = calloc_(sizeof(trie) * (gpos + 1));
+	gbuf = calloc_(sizeof(*gbuf) * (gpos + 1));
+	aux = calloc_(sizeof(*aux) * apos);
 	gpos = 0;
+	apos = 0;
 	gbuf_fill(&base);
 
-/*
-typedef struct {
-	uint64_t keys[2];
-	uint64_t terms[2];
-	int *next;
-	int *data;
-} trie;
-*/
+	printf("trie instr_tbase[] = {\n");
 	for (int i = 0; i <= gpos; i++) {
-		printf("{{%lu,%lu},{%lu,%lu},", gbuf[i].keys[0], gbuf[i].keys[1], gbuf[i].terms[0], gbuf[i].terms[1]);
-		int lim = __builtin_popcountl(gbuf[i].keys[0]) + __builtin_popcountl(gbuf[i].keys[1]);
-		if (lim == 0) {
-			printf("NULL");
-		} else {
-			printf("(int[]){");
-			for (int j = 0; j < lim; j++) {
-				printf("%d", gbuf[i].next[j]);
-				if (j != lim - 1) {
-					printf(",");
-				}
-			}
-			printf("}");
-		}
-		lim = __builtin_popcountl(gbuf[i].terms[0]) + __builtin_popcountl(gbuf[i].terms[1]);
-		if (lim != 0) {
-			printf(",(int[]){");
-			for (int j = 0; j < lim; j++) {
-				printf("%d", gbuf[i].data[j]);
-				if (j != lim - 1) {
-					printf(",");
-				}
-			}
-			printf("}");
-		}
-		printf("},\n");
+		printf("\t{{%luL,%luL},{%luL,%luL},%d,%d},\n", gbuf[i].keys[0], gbuf[i].keys[1], gbuf[i].terms[0], gbuf[i].terms[1], gbuf[i].next, gbuf[i].data);
 	}
-
-	trie *tbase = gbuf;
-	int pos = 0;
-	char *s = "LB ";
-	char here = s[0];
-	while (*s) {
-		if (here < 'a') {
-			here += 'a' - 'A';
-		}
-		char next = s[1];
-		if (next == ' ' || next == '\t' || next == '\0') {
-			int *res = trie_term(tbase + pos, here);
-			if (res) {
-				printf("res: %d\n", *res);
-			} else {
-				puts("no result");
-			}
-			break;
-		}
-		pos = trie_next(tbase + pos, here);
-		if (pos < 0) {
-			puts("no result (early)");
-			break;
-		}
-		here = next;
-		s++;
+	printf(
+		"};\n"
+		"\n"
+		"int instr_tbase_auxiliary[] = {\n"
+	);
+	for (int i = 0; i < apos; i++) {
+		printf("\t%d,\n", aux[i]);
 	}
-
-	/*
-	trie *t = &gbuf[0];
-#define S(t, j) printf("%064lb %064lb\n", (t)->j[0], (t)->j[1])
-	S(t, keys);
-	printf("%d\n", t->next[0]);
-	_print_keys(t->keys);
-	puts("");
-	S(gbuf + t->next[0], keys);
-
-	int n = trie_next(t, 's');
-	printf("%d\n", n);
-	t = gbuf + n;
-	S(t, keys);
-	_print_keys(t->keys);
-	puts("");
-	*/
+	printf("};\n");
 
 	return 0;
 }
